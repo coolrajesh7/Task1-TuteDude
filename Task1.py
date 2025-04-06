d4a45c14cd119536ae1503b81fd63df448782b77
coolrajesh7 <52200943+coolrajesh7@users.noreply.github.com>
2025-04-06 12:17:13 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,18 @@
+'''
+Task 1
+Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+'''
+first_num = int(input("Enter the first number:"))
+second_num = int(input("Enter the second number:"))
+
+print('Addition: ',str(first_num + second_num))
+print('Subtraction: ',str(first_num - second_num))
+print('Multiplication: ',str(first_num * second_num))
+print('Division: ',str(first_num / second_num))
---
+++ Task2.py
@@ -0,0 +1,10 @@
+'''
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+first_name = input('Enter your first name:')
+second_name = input('Enter your second name:')
+print('Hello, '+ first_name+' '+ second_name+ '!'+' Welcome to the Python program')
